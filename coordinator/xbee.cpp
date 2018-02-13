dbe3b23f10c2315115a7e772c3920b0b62d64937
Philippe Garneau <garp2405@usherbrooke.ca>
2018-02-12 22:44:55 -0500

    almost done coord

--- coordinator/xbee.cpp
+++ coordinator/xbee.cpp
@@ -5,6 +5,7 @@ Serial xbee(p13, p14);
 Serial pc(USBTX, USBRX);
 DigitalOut xbeeResetPin(p8);
 const char MAC_MASK[]={0,0,0,0,0,0,0,0};
+int swag[128];
 
 uint8_t get_MSB(uint16_t data) {
     uint8_t MSB = (data >> 8) & 0x00FF;
@@ -38,9 +39,29 @@ void readXbee(int *readData) {
 		}
 		pc.printf("%02X ", received_char);
 		counter++;
-		wait_ms(50);
+		wait_ms(5);
 	}
 	pc.printf("\n\rDone receiving\n\r");
+}
+
+void confirmCommand(int command) {
+	int counter = 0;
+	memset(swag, 0, 128);
+	
+	while (!xbee.readable()) {
+	}
+	while (xbee.readable()) {
+		uint8_t received_char = xbee.getc();
+		swag[counter] = received_char;
+		//pc.printf("%02X ", received_char);
+		counter++;
+		wait_ms(5);
+	}
+	if (swag[3] == command) {
+		pc.printf("VALID\n\r");
+	} else {
+		pc.printf("INVALID\n\r");
+	}
 }
 
 
@@ -50,9 +71,10 @@ void sendXbee(char *data, int dataLength) {
     
     for (uint8_t index = 0; index < dataLength; index++) {
         xbee.putc(data[index]);
-        pc.printf("%02X ", data[index]);
-        wait_ms(25);
+        //pc.printf("%02X ", data[index]);
+        //wait_ms(15);
     }
+	pc.printf("\n\r");
    
 }
 
@@ -81,35 +103,8 @@ void sendCommand(char *command, char *data, uint8_t dataLength) {
     
     setChecksum(commandFrame);
     
-    sendXbee(commandFrame, dataLength + MIN_COMMAND_FRAME_SIZE);  
-}
-void sendTransmit(char *data, int dataLength) {
-	 char commandFrame[128];
-    commandFrame[START_INDEX] = START;
-    commandFrame[LENGTH_MSB_INDEX] = get_MSB(dataLength +TRANSMIT_MIN_SIZE);
-    commandFrame[LENGTH_LSB_INDEX] = get_LSB(dataLength +TRANSMIT_MIN_SIZE);
-    commandFrame[COMMAND_ID_INDEX] = ZIGBEE_TRANSMIT_ID;
-    commandFrame[FRAME_ID_INDEX] = FRAME_ID;
-	  memcpy(&commandFrame[5], MAC_MASK, MAC_SIZE);
-		commandFrame[13]=0xFF;
-		commandFrame[14]= 0xFE;
-		commandFrame[15]= 0x00;
-		commandFrame[16]= 0x00;
-	
-	
-		for(int i = 0; i<dataLength; ++i){
-		pc.printf("meme copy index %i = %c \n\r",i,data[i]);
-	}
-		memcpy(&commandFrame[17], data, dataLength);
-	int commandSize = 17 + dataLength;
-	for(int i = 0; i<commandSize; ++i){
- pc.printf("index[ %i ] = %02x \n\r",i,commandFrame[i]);
-	}
-	  pc.printf("le data length envoyer is %i + %i\n\r",dataLength , MIN_COMMAND_FRAME_SIZE);
-		setChecksum(commandFrame);
-		commandSize++;
-    sendXbee(commandFrame, commandSize);  
-    
+    sendXbee(commandFrame, dataLength + MIN_COMMAND_FRAME_SIZE);
+	confirmCommand(0x88);
 }
 
 void sendCommandRequest(char *command, char *mac, int parameter, int dataLength) {
@@ -118,7 +113,7 @@ void sendCommandRequest(char *command, char *mac, int parameter, int dataLength)
 	commandFrame[LENGTH_MSB_INDEX] = get_MSB(dataLength + MIN_COMMAND_REQUEST_FRAME_DATA_SIZE);
 	commandFrame[LENGTH_LSB_INDEX] = get_LSB(dataLength + MIN_COMMAND_REQUEST_FRAME_DATA_SIZE);
 	commandFrame[COMMAND_ID_INDEX] = REMOTE_AT_COMMAND;
-	commandFrame[FRAME_ID_INDEX] = FRAME_ID;
+	commandFrame[FRAME_ID_INDEX] = 0x00;
 	memcpy(&commandFrame[5], mac, 8);
 	commandFrame[13] = 0xFF;
 	commandFrame[14] = 0xFE;
@@ -129,6 +124,8 @@ void sendCommandRequest(char *command, char *mac, int parameter, int dataLength)
 	setChecksum(commandFrame);
 	
 	sendXbee(commandFrame, dataLength + MIN_COMMAND_REQUEST_FRAME_SIZE);
+	wait_ms(25);
+	//confirmCommand(0x97);
 }
 
 void sendRemoteCommand() {
@@ -140,9 +137,6 @@ void initXbee(char *panId, int *readData) {
     resetXbee();
 	readXbee(readData);
     sendCommand("ID", panId, 8);
-	readXbee(readData);
     sendCommand("WR", 0, 0);
-	readXbee(readData);
     sendCommand("AC", 0, 0);
-	readXbee(readData);
 }
